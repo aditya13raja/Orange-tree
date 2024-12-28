612e5439b80fb5c8e3ea39834c92bf1935baf4c8
aditya13raja <aditya13raja@gmail.com>
2024-12-28 14:56:23 +0530

    Create routes and pages in client

---
+++ client/src/pages/ProductPage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const ProductPage: React.FC<> = () => {
+
+    return (
+        <>
+            <h1>ProductPage</h1>
+        </>
+    )
+}
+
+export default ProductPage;
